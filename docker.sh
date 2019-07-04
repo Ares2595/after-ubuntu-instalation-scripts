ae892f8de70000146cf16a8c7115e4b13eb369ba
Daniel Serkowski <dserkowski@soldevelo.com>
2019-07-04 19:39:02 +0200

    Updated docker-compose version

--- docker.sh
+++ docker.sh
@@ -25,7 +25,7 @@ sudo systemctl enable docker
 sudo groupadd docker
 sudo usermod -aG docker `whoami`
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
